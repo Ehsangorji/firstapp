6035092b2a25edc61a79a889fa3719a4dca7ab91
Ehsangorji <ehsan.gorji1998@gmail.com>
2021-08-16 11:20:29 +0430

    commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+// import './App.css';
+  import './App.css';
+
+function App() {
+  return (    <div >
+
+    <div  class="flex-container row0" >
+       <div>   Hello  </div>
+    </div>
+  
+  
+    <div class="flex-container row1">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div> 
+    </div>
+  
+    <div class="flex-container row2">
+    <div>4</div>
+    <div>5</div>
+  
+  </div>
+  
+   <div class="flex-container row3" >
+    <div>c</div>
+    <div>c</div>
+    <div>c</div>
+   </div>
+
+   <div class="flex-container row4" >
+    <div>+</div>
+    <div>+</div>
+    <div>+</div>
+    <div>+</div>
+   </div>
+   <div class="flex-container row5" >
+
+    <div>+</div>
+    <div>+</div>
+    <div>+</div>
+    <div>+</div>
+   </div>
+  
+  </div>  
+  );
+}
+
+  
+
+
+
+
+export default App;
